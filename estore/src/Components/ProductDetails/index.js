a8d22474734abdc7c063ac4e9b91e171dc2be5cf
Anko2136 <am.kontaratou@gmail.com>
2024-11-13 11:27:37 +0000

    added product details inside ProdectDetails component - frontend

--- estore/src/Components/ProductDetails/index.js
+++ estore/src/Components/ProductDetails/index.js
@@ -1,10 +1,44 @@
 import React from 'react'
 import "./_product-details.scss"
+import { useLocation } from 'react-router-dom'
 
 const ProductDetails = () => {
+    const location = useLocation();
+
+    console.log(location.state.product_img)
   return (
     <div>
-        <h1>ProductDetails</h1>
+        <div>
+            <div>
+                <img src = {require(`../../assets/images/${location.state.product_img}`)}/>
+            </div>
+            <div>
+                <span>{location.state.product_name}</span>
+            </div>
+            <div className='product-rating'>
+                <i className='fa fa-star'/>
+                <i className='fa fa-star'/>
+                <i className='fa fa-star'/>
+                <i className='fa fa-star'/>
+                <i className='fa fa-star'/>
+            </div>
+            <div>
+                MRP : <span>{location.state.price}</span>
+                <div>Incluseive of all taxes.</div>
+            </div>
+            <div>
+                <span>Some product description</span>
+
+                <div>
+                    <div>
+                        <div>
+                            <i className='fa fa-shopping-cart'/>
+                        </div>
+                        <div>Add to cart</div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
