68cee9fdaba3b13e967f5e184a536e0b31da46b2
Anko2136 <am.kontaratou@gmail.com>
2024-11-13 11:45:40 +0000

    added css

--- estore/src/Components/ProductDetails/index.js
+++ estore/src/Components/ProductDetails/index.js
@@ -1,46 +1,50 @@
-import React from 'react'
-import "./_product-details.scss"
-import { useLocation } from 'react-router-dom'
+import { useLocation } from "react-router-dom";
+import './_product-details.scss';
 
-const ProductDetails = () => {
+const ProductDetails = ()=>{
     const location = useLocation();
 
-    console.log(location.state.product_img)
-  return (
-    <div>
-        <div>
-            <div>
-                <img src = {require(`../../assets/images/${location.state.product_img}`)}/>
-            </div>
-            <div>
-                <span>{location.state.product_name}</span>
-            </div>
-            <div className='product-rating'>
-                <i className='fa fa-star'/>
-                <i className='fa fa-star'/>
-                <i className='fa fa-star'/>
-                <i className='fa fa-star'/>
-                <i className='fa fa-star'/>
-            </div>
-            <div>
-                MRP : <span>{location.state.price}</span>
-                <div>Incluseive of all taxes.</div>
-            </div>
-            <div>
-                <span>Some product description</span>
+    console.log(location);
 
-                <div>
-                    <div>
-                        <div>
-                            <i className='fa fa-shopping-cart'/>
+    return(
+        <div>
+            <div className="row container my-5 product-details-container">
+                <div className="col-5 product-img-container">
+                    <img src={require('../../assets/images/'+location.state.product_img)}/>
+                </div>
+                <div className="col-7 product-info">
+                    <span id="product-name"> {location.state.product_name} </span>
+
+                    <div className="rating-container">
+                        <i className="fa fa-star"/>
+                        <i className="fa fa-star"/>
+                        <i className="fa fa-star"/>
+                        <i className="fa fa-star"/>
+                        <i className="fa fa-star"/>
+                    </div>
+                    <hr/>
+                    <div className="product-price">
+                        MRP : <span className="price"> ${location.state.price} </span>
+                        <div style={{fontSize:"0.8em"}}> Inclusive of all taxes. </div>
+                    </div>
+                    <div className="my-3 product-description">
+                        <span> Some Product Description Given Here... </span>
+                    </div>
+
+                    <div className="my-5">
+                        <div className="btn btn-warning cart-button">
+                            <div className="cart-icon-container">
+                                <i className="fa fa-shopping-cart"/>
+                            </div>
+                            <div className="cart-text-container">
+                                <p> Add to Cart </p>
+                            </div>
                         </div>
-                        <div>Add to cart</div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
-export default ProductDetails
+export default ProductDetails;
